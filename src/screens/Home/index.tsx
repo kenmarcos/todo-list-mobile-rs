900dd7c88416de75f89504f6b989a53f2d78875c
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-05-28 18:14:33 -0300

    feat: add task input and task list empty

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,60 @@
-import { Text, View } from "react-native";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 import { Header } from "../../components/Header";
+import { PlusCircle } from "phosphor-react-native";
 
 export const Home = () => {
   return (
     <View style={styles.homeContainer}>
       <Header />
 
-      <Text>Home</Text>
+      <View style={styles.taskCreationContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Adicione uma nova tarefa"
+          placeholderTextColor="#808080"
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <PlusCircle size={16} color="#F2F2F2" />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tasksContainer}>
+        <View style={styles.header}>
+          <View style={styles.taskInfo}>
+            <Text style={[styles.taskInfoText, styles.createdTasks]}>
+              Criadas
+            </Text>
+            <View style={styles.badge}>
+              <Text style={styles.badgeText}>0</Text>
+            </View>
+          </View>
+
+          <View style={styles.taskInfo}>
+            <Text style={[styles.taskInfoText, styles.completedTasks]}>
+              Concluídas
+            </Text>
+            <View style={styles.badge}>
+              <Text style={styles.badgeText}>0</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.emptyTasksContainer}>
+          <Image source={require("../../assets/clipboard.png")} />
+
+          <View style={styles.message}>
+            <Text style={[styles.messageText, styles.mainMessageText]}>
+              Você ainda não tem tarefas cadastradas
+            </Text>
+
+            <Text style={styles.messageText}>
+              Crie tarefas e organize seus itens a fazer
+            </Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -5,4 +5,81 @@ export const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#1a1a1a",
   },
+
+  taskCreationContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 4,
+    marginHorizontal: 24,
+    marginTop: -27,
+  },
+  input: {
+    height: 54,
+    flex: 1,
+    backgroundColor: "#262626",
+    borderWidth: 1,
+    borderColor: "#0D0D0D",
+    borderRadius: 6,
+    padding: 16,
+  },
+  button: {
+    backgroundColor: "#1E6F9F",
+    borderRadius: 6,
+    width: 52,
+    height: 52,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  tasksContainer: {
+    marginTop: 32,
+    marginHorizontal: 24,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  taskInfo: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  taskInfoText: {
+    fontWeight: "bold",
+  },
+  createdTasks: {
+    color: "#4EA8DE",
+  },
+  completedTasks: {
+    color: "#8284FA",
+  },
+  badge: {
+    backgroundColor: "#333333",
+    width: 30,
+    borderRadius: 999,
+    alignItems: "center",
+  },
+  badgeText: {
+    color: "#D9D9D9",
+    fontSize: 12,
+    fontWeight: "bold",
+  },
+
+  emptyTasksContainer: {
+    borderTopWidth: 2,
+    borderTopColor: "#333333",
+    marginTop: 20,
+    minHeight: 208,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 16,
+  },
+  message: {},
+  messageText: {
+    color: "#808080",
+    textAlign: "center",
+  },
+  mainMessageText: {
+    fontWeight: "bold",
+  },
 });
